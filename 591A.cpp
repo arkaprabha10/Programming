374c20fce3c8d4fd5ab5e084607b86397e1076f8
Arkaprabha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-01-01 19:23:06 +0530

    Add files via upload

---
+++ 591A.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{   
+    double l,p,q;
+    cin>>l>>p>>q;
+    cout<<(l*p)/(p+q);
+    return 0;
+}
