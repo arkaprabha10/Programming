9d669f98cff097c8de3203b2b9fdab3644264dd6
Arkaprabha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-01-07 21:09:37 +0530

    Add files via upload

---
+++ 554A.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{   
+    string s;
+    cin>>s;
+    cout<<(s.size()+1)*26 - s.size();
+return 0; 
+}
+
